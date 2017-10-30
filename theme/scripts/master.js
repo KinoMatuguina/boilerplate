d990a4645295dbea62b0fab0da0aacc58e5c21c3
kinom <kino.bacani.matuguina@gmail.com>
2017-10-30 09:24:27 +0800

    [INITIAL]

---
+++ theme/scripts/master.js
@@ -0,0 +1,17 @@
+/*!
+ * [Project Name]
+ */
+
+var KINO = (function() {
+	'use strict';
+
+	function init() {
+		// Your code here
+	}
+
+	return {
+		init: init
+	};
+}());
+
+jQuery(document).ready(function($) { KINO.init(); });
